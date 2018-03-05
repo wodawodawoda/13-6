313ae1496598223b2842ecac9be0f074113a136f
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-05 21:59:05 +0100

    Add .gitignore

--- program.js
+++ program.js
@@ -1,17 +1,26 @@
-const OSinfo = require('./OSinfo');
+const OSinfo = require('./OSinfo'),
+	EventEmitter = require('events').EventEmitter;
+	
+const emmiter = new EventEmitter();
+emmiter.on('beforeCommand', function(instruction) {
+	console.log(`You wrote: ${instructions} trying to run command.`);
+});
+emmiter.on('afterCommand', function() {
+	console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
 	const input = process.stdin.read();
 	if (input !== null) {
 		const instruction = input.toString().trim();
+		emmiter.emit('beforeCommand', instruction);
 		switch(instruction) {
 		 	case '/exit':
 				process.stdout.write('Quitting app!\n');
 				process.exit();
 		 		break;
 		 	case '/info' :
-				// process.stdout.write(`\nLanguage: ${process.env.LANGUAGE}\nNode version: ${process.versions.node}\n`);
 			 	console.log(`Language: ${process.env.LANGUAGE}`);
 			 	console.log(`Node version: ${process.versions.node}`);
 			 	break;
@@ -21,5 +30,6 @@ process.stdin.on('readable', function () {
 		 	default:
 				process.stderr.write('Wrong instruction\n');
 		}
+		emmiter.emit('afterCommand');
 	}
 });
